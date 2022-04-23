786198a47f36bb1f062de89f931ed0ea59dd0cd8
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-04-23 13:36:59 +0530

    x = list(map(int,input().split()))
    p,r,t = x[0],x[1],x[2]
    
    
    ci = p*((1 + r/100)**t)
    print("{:.2f}".format(ci))

--- Compound_interest.py
+++ Compound_interest.py
@@ -1,8 +1,7 @@
-x = list(map(float,input().split()))
+x = list(map(int,input().split()))
 
-p = x[0]
-r = x[1]
-t = x[2]
-ci = p * ( 1 + ( r / 100 ) ) ** t 
+p,r,t = x[0],x[1],x[2]
 
-print('{:.2f}'.format(ci))
+
+ci = p*((1 + r/100)**t)
+print("{:.2f}".format(ci))
