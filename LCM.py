831d0e8f684340b69ea90693563cbcfe64e0c10d
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-04-23 21:00:46 +0530

    x = list(map(int,input().split()))
    if x[0]>x[1]:
        max_ = x[0]
    else:
        max_ = x[1]
    
    for i in range(1,max_):
        if x[0]%i==0 and x[1]%i==0:
            hcf = i
    
    lcm = (x[0]*x[1])/hcf
    print(int(lcm))

---
+++ LCM.py
@@ -0,0 +1,14 @@
+x = list(map(int,input().split()))
+
+if x[0]>x[1]:
+    max_ = x[0]
+else:
+    max_ = x[1]
+
+for i in range(1,max_):
+    if x[0]%i==0 and x[1]%i==0:
+        hcf = i
+
+lcm = (x[0]*x[1])/hcf
+print(int(lcm))
+    
