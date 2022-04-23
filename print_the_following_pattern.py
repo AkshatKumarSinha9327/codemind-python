54b7ce3355b1b54ad73cdf9bf89b334734d81f9b
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-04-23 20:26:44 +0530

    n = int(input()) a = n+ 64
    for i in range(a,64,-1):
        for j in range(64,i):
            print(chr(i),end=' ')
        print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,7 @@
 n = int(input())
+a = n+ 64
 
-for i in range(n,0,-1):
-    for j in range(n,0,-1):
-        print(j,end=' ')
+for i in range(a,64,-1):
+    for j in range(64,i):
+        print(chr(i),end=' ')
     print()
