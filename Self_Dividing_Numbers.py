2e40f9b55cb5bd1e6d2838b492eebff30ae007b0
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-06-28 20:00:55 +0530

    def sdn(n):     temp =n     while temp:         z = temp%10         if z==0:             return False         elif n%(z)!=0:             return False         temp//=10     return True
    a = int(input())
    b = int(input())
    
    for i in range(a,b+1):
        if sdn(i):
            print(i,end=' ')

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,18 +1,17 @@
-a,b = int(input()),int(input())
-flag = 0
-for i in range(a,b+1):
-    temp = i;
-    while(temp>0):
-        a = temp%10
-        if a!=0:
-            if (i%a==0):
-                flag =1;
-            else:
-                flag =0
-                break
-        else:
-            flag=0
-            break
+def sdn(n):
+    temp =n
+    while temp:
+        z = temp%10
+        if z==0:
+            return False
+        elif n%(z)!=0:
+            return False
         temp//=10
-    if flag!=0:
+    return True
+
+a = int(input())
+b = int(input())
+
+for i in range(a,b+1):
+    if sdn(i):
         print(i,end=' ')
