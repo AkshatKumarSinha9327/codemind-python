85749143250e5a8234d69a3ae34b045504855644
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-15 02:01:48 +0530

    n = int(input()) c=0 x = list(map(int,input().split())) temp = [] for i in x:     if i not in temp and i%2==0:         c+=1         temp.append(i) print(c)

--- unique_even_count.py
+++ unique_even_count.py
@@ -1,7 +1,9 @@
 n = int(input())
-x = set(map(int,input().split()))
 c=0
+x = list(map(int,input().split()))
+temp = []
 for i in x:
-    if i%2==0:
+    if i not in temp and i%2==0:
         c+=1
+        temp.append(i)
 print(c)
