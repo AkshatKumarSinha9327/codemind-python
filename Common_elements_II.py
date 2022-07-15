2591b9c44751c3d8dd59663429ae43c7e42ac19f
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-15 12:02:46 +0530

    n,m = map(int,input().split())
    x = list(map(int,input().split()))
    y = list(map(int,input().split()))
    temp = []
    for i in x:
        if i not in y and i not in temp:
            temp.append(i)
    for i in y:
        if i not in  x and i not in temp:
            temp.append(i)
            
    for i in temp:
        print(i,end=' ')

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,17 +1,14 @@
-m,n = map(int,input().split())
+n,m = map(int,input().split())
 
 x = list(map(int,input().split()))
 y = list(map(int,input().split()))
-
-# print(x,'
-',y)
 temp = []
 for i in x:
-    if (i not in y) and (i not in temp):
+    if i not in y and i not in temp:
         temp.append(i)
-        print(i,end=' ')
-
 for i in y:
-    if (i not in x) and (i not in temp):
+    if i not in  x and i not in temp:
         temp.append(i)
-        print(i,end=' ')
+        
+for i in temp:
+    print(i,end=' ')
