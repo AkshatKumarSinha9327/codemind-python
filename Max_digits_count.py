6251ddd16bccd911cf8e147c38759de7d7072d9e
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-15 14:52:05 +0530

    def leni(n):     c = 0     while n:         c+=1         n//=10     return c
    n = int(input())
    x = list(map(int,input().split()))
    mc = 0
    for i in x:
        if leni(i)>mc:
            mc = leni(i)
    c=0
    for i in x:
        if leni(i)==mc:
            c+=1
    print(c)

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,14 +1,18 @@
+def leni(n):
+    c = 0
+    while n:
+        c+=1
+        n//=10
+    return c
+
 n = int(input())
-
 x = list(map(int,input().split()))
-
-max_ = len(str(x[0]))
+mc = 0
 for i in x:
-    if len(str(i))>max_:
-        max_=len(str(i))
-
-c = 0
+    if leni(i)>mc:
+        mc = leni(i)
+c=0
 for i in x:
-    if max_==len(str(i)):
+    if leni(i)==mc:
         c+=1
 print(c)
