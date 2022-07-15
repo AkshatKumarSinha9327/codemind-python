9bb8a2a72d7d2a5cb0fe1738e3527efbf59f06ee
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-15 19:40:35 +0530

    s = input() v = 'aeiouAEIOU' c=0 temp = [] for  i in s:     if i in v and i not  in temp:         print(i,end=' ')         temp.append(i)         c=1 if c==0:     print(-1)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -2,10 +2,10 @@ s = input()
 v = 'aeiouAEIOU'
 c=0
 temp = []
-for i in s:
-    if i in v and i not in temp:
+for  i in s:
+    if i in v and i not  in temp:
         print(i,end=' ')
         temp.append(i)
         c=1
-if not bool(c):
+if c==0:
     print(-1)
