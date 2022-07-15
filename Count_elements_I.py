4fd82966bc6ded9495418589d9384eeedcbbe0b4
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-15 10:37:20 +0530

    n,m = map(int,input().split())
    x = list(map(int,input().split()))
    l = list(map(int,input().split()))
    
    c=0
    temp = []
    for i in x:
        if i in l and i not in temp:
            temp.append(i)
            c+=1
    print(c)

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,14 +1,12 @@
-m,n = map(int,input().split())
+n,m = map(int,input().split())
 
 x = list(map(int,input().split()))
-y = list(map(int,input().split()))
+l = list(map(int,input().split()))
 
-# print(x,'
-',y)
 c=0
 temp = []
 for i in x:
-    if (i in y) and (i not in temp):
+    if i in l and i not in temp:
         temp.append(i)
         c+=1
 print(c)
