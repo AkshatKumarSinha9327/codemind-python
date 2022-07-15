18a17bcc580cca89351700d9036e16b01641f637
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-15 23:33:27 +0530

    s = input().lower() temp = [] for i in sorted(s):     if s.count(i)==1 and i not in temp and i!=' ':         print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,6 +1,5 @@
-s=list(set(input().lower()))
-#s.remove(' ')
-s.sort()
-for i in s:
-    if i!=' ':
+s = input().lower()
+temp = []
+for i in sorted(s):
+    if s.count(i)==1 and i not in temp and i!=' ':
         print(i,end='')
