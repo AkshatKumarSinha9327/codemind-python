a7fba69ccd2a0d7daf6ac15c7c93bb4175beb500
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-17 13:20:38 +0530

    n = int(input()) x = list(map(int,input().split())) s=c=0 temp = [] for i in x:     if i not in temp and i==x.count(i):         temp.append(i)         s+=i         c+=1 if c==0:     print(-1) else:     print('{:.2f}'.format(s/c))

--- Average_of_super_elements.py
+++ Average_of_super_elements.py
@@ -1,14 +1,13 @@
 n = int(input())
-
 x = list(map(int,input().split()))
-s = 0
+s=c=0
 temp = []
 for i in x:
-    if x.count(i)==i and i not in temp:
-        s+=i
+    if i not in temp and i==x.count(i):
         temp.append(i)
-
-if len(temp)==0:
+        s+=i
+        c+=1
+if c==0:
     print(-1)
 else:
-    print('{:.2f}'.format(s/len(temp)))
+    print('{:.2f}'.format(s/c))
