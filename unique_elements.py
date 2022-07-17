5a7b02cb89e6c703253989cb6520ca944bdc9503
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-17 12:03:52 +0530

    n = int(input()) x = list(map(int,input().split())) # print(x) temp = [] for i in x:     if i not in temp:         print(i,end=' ')         temp.append(i)

--- unique_elements.py
+++ unique_elements.py
@@ -1,8 +1,8 @@
 n = int(input())
 x = list(map(int,input().split()))
+# print(x)
 temp = []
-
 for i in x:
     if i not in temp:
-        temp.append(i)
         print(i,end=' ')
+        temp.append(i)
