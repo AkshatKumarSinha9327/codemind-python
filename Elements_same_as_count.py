f55c5f693192c5071ccb473e69907b1dcb53d6bb
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-17 13:30:20 +0530

    n = int(input()) x = list(map(int,input().split())) temp = [] for i in x:     if i not in temp and i==x.count(i):         temp.append(i)         print(i,end=' ') if len(temp)==0:     print(-1)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,15 +1,9 @@
 n = int(input())
-
 x = list(map(int,input().split()))
-
 temp = []
-
 for i in x:
-    if x.count(i)==i and i not in temp:
+    if i not in temp and i==x.count(i):
         temp.append(i)
-
+        print(i,end=' ')
 if len(temp)==0:
     print(-1)
-else:
-    for i in temp:
-        print(i,end=' ')
