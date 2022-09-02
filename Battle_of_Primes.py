e32453ea02d1cbac13c6302f76745ebd759da4f0
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-09-02 21:52:17 +0530

    def prime(n):     if n==0 or n==1:         return False     for i in range(2,int(n**.5)+1):         if n%i==0:             return False     return True n1=int(input()) n2=int(input()) k=n1+n2 for i in range(1,100000000):     if prime(i+k):         print(i)         break

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,14 +1,15 @@
 def prime(n):
-    if n==1:
+    if n==0 or n==1:
         return False
-    for i in range(2,int(n**0.5)+1):
+    for i in range(2,int(n**.5)+1):
         if n%i==0:
             return False
     return True
-
-a,b = int(input()),int(input())
-
-for i in range(1,10000000):
-    if prime(a+b+i):
+n1=int(input())
+n2=int(input())
+k=n1+n2
+for i in range(1,100000000):
+    if prime(i+k):
         print(i)
         break
+        
